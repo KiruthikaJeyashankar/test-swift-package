9162ac0621cb8053ce497dd66f9643e725c17b31
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-03-18 14:34:50 +0530

    2nd

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,8 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 public struct MyLibrary {
-    public init(){}
-    
     public func multiple(a: Int, b:Int) -> Int {
         return a+b;
     }
